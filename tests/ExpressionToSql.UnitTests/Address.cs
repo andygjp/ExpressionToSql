cf72a41509ccc11cc3d7034797b650d32f3167ff
Andrew Pattison <andygjp@hotmail.com>
2016-05-29 08:44:08 +0100

    First test.

---
+++ tests/ExpressionToSql.UnitTests/Address.cs
@@ -0,0 +1,11 @@
+namespace ExpressionToSql.UnitTests
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string DeliveryName { get; set; }
+        public string Address1 { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ tests/ExpressionToSql.UnitTests/SqlTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ExpressionToSql.UnitTests
+{
+    using ExpressionToSql;
+    using FluentAssertions;
+    using Xunit;
+
+    public class SqlTests
+    {
+        [Fact]
+        public void Simple_case_should_produce_select()
+        {
+            string actual = Sql.Select((Address x) => x.Id).ToString();
+            actual.Should().Be("SELECT Id FROM [Address]");
+        }
+    }
+}
