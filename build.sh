80a5b59a0af271eed142e246428b5248f4cbffa9
Andrew Pattison <andygjp@hotmail.com>
2018-12-30 16:06:02 +0000

    chore: updated output paths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 dotnet --version
 dotnet restore
 dotnet test tests/ExpressionToSql.UnitTests
-dotnet pack src/ExpressionToSql --output output/ExpressionToSql --configuration Release
-dotnet pack src/ExpressionToSql.Dapper --output output/ExpressionToSql_Dapper --configuration Release
+dotnet pack src/ExpressionToSql --output ../../output/ExpressionToSql --configuration Release
+dotnet pack src/ExpressionToSql.Dapper --output ../../output/ExpressionToSql_Dapper --configuration Release
